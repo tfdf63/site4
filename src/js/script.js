3653c1635017ae42eb137a1772a3a8769b9382ba
Slava <tfdf63@yandex.ru>
2022-08-16 17:22:23 +0400

    slider tini slider

--- src/js/script.js
+++ src/js/script.js
@@ -1,17 +1,33 @@
-$(document).ready(function () {
-    $('.carousel__inner').slick({
-        speed: 300,
-        adaptiveHeight: true,
-        prevArrow: '<button type="button" class="slick-prev"><img src="../icons/lefts.svg"></button>',
-        nextArrow: '<button type="button" class="slick-next"><img src="../icons/rights.svg"></button>',
-        responsive: [
-            {
-                breakpoint: 992,
-                settings: {
-                    dots: true,
-                    arrows: false
-                }
-            }
-        ]
-    });
+// $(document).ready(function () {
+//     $('.carousel__inner').slick({
+//         speed: 300,
+//         adaptiveHeight: true,
+//         prevArrow: '<button type="button" class="slick-prev"><img src="../icons/lefts.svg"></button>',
+//         nextArrow: '<button type="button" class="slick-next"><img src="../icons/rights.svg"></button>',
+//         responsive: [
+//             {
+//                 breakpoint: 992,
+//                 settings: {
+//                     dots: true,
+//                     arrows: false
+//                 }
+//             }
+//         ]
+//     });
+// });
+
+const slider = tns({
+    container: '.carousel__inner',
+    items: 1,
+    slideBy: 'page',
+    autoplay: false,
+    controls: false
+});
+
+document.querySelector('.prev').addEventListener('click', function () {
+    slider.goTo('prev');
+});
+
+document.querySelector('.next').addEventListener('click', function () {
+    slider.goTo('next');
 });
